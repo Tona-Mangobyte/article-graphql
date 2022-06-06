677b915c4ca727cf1f520df2d55b0712e1865402
Tona <tona@mango-byte.com>
2022-06-06 15:56:38 +0700

    impl list article in page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,26 +2,33 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+        <v-card-title>
+          <div class="display-1">Article List</div>
+          <v-spacer />
+          <v-btn
+            to="/create"
+            class="mx-2"
+            fab
+            small
+            dark
+            color="indigo"
+          >
+            <v-icon dark> mdi-plus </v-icon>
+          </v-btn>
         </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-          <v-spacer />
-          <v-btn color="primary" @click="onCreateArticle"> Create Article </v-btn>
-        </v-card-actions>
+        <v-data-table
+          :headers="headers"
+          :items="records"
+          :server-items-length="pagination.total"
+          :items-per-page.sync="limit"
+          :options.sync="options"
+          :footer-props="{
+        itemsPerPageOptions: [10, 30, 50, 100, -1],
+      }"
+          loading-text="Loading..."
+          class="v-data-table elevation-1"
+        >
+        </v-data-table>
       </v-card>
     </v-col>
   </v-row>
@@ -31,13 +38,26 @@
 import { GET_ARTICLES, CREATE_ARTICLE } from '~/graphql';
 export default {
   name: 'IndexPage',
-  async fetch({ app }) {
-    const client = app.apolloProvider.defaultClient
+  data() {
+    return {
+      limit: 10,
+      records: [],
+      pagination: { page: 1, limit: 10 },
+      options: {},
+      headers: [
+        { text: 'Id', value: 'id', sortable: false },
+        { text: 'Title', value: 'title', sortable: false },
+      ]
+    }
+  },
+  async mounted() {
+    const client = this.$nuxt.context.app.apolloProvider.defaultClient
     const res = await client.query({
       query: GET_ARTICLES,
       variables: { page: 1, limit: 10 }
     })
-    console.info(res.data)
+    this.records = res.data.getAllArticles.items
+    this.pagination = res.data.getAllArticles.pagination
   },
   methods: {
     async onCreateArticle() {
