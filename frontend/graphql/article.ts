092d2203ff98d2621647e0d4cba4c24d5c288609
Tona <tona@mango-byte.com>
2022-06-06 14:04:36 +0700

    impl create article

--- frontend/graphql/article.ts
+++ frontend/graphql/article.ts
@@ -7,3 +7,10 @@ export const GET_ARTICLES = gql` query($page: Int!, $limit: Int!){
     content
   }
 }`
+export const CREATE_ARTICLE = gql` mutation($title: String!, $content: String!){
+  createArticle(article: {title: $title, content: $content}) {
+    id,
+    title,
+    content
+  }
+}`
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
@@ -16,64 +12,15 @@
             applications.
           </p>
           <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
             Thank you for developing with Vuetify and I look forward to bringing
             more exciting features in the future.
           </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-spacer />
+          <v-btn color="primary" @click="onCreateArticle"> Create Article </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -81,7 +28,7 @@
 </template>
 
 <script>
-import { GET_ARTICLES } from '~/graphql';
+import { GET_ARTICLES, CREATE_ARTICLE } from '~/graphql';
 export default {
   name: 'IndexPage',
   async fetch({ app }) {
@@ -92,5 +39,15 @@ export default {
     })
     console.info(res.data)
   },
+  methods: {
+    async onCreateArticle() {
+      const client = this.$nuxt.context.app.apolloProvider.defaultClient
+      const res = await client.mutate({
+        mutation: CREATE_ARTICLE,
+        variables: { title: 'the news', content: 'the content' }
+      })
+      console.info(res.data)
+    }
+  }
 }
 </script>
